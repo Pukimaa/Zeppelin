a51b96bd09ad8ac5dc408113c31bcb2112901f39
Pukimaa <me@pukima.site>
2024-08-30 14:13:29 +0200

    maybe fix something?

--- backend/src/plugins/ModActions/ModActionsPlugin.ts
+++ backend/src/plugins/ModActions/ModActionsPlugin.ts
@@ -140,27 +140,12 @@ const defaultOptions = {
   ],
 };
 
-/**
- * Config preprocessor to fix values
- */
-const configPreprocessor = (options) => {
-  if (options.config?.reason_aliases) {
-    options.config.reason_aliases = Object.fromEntries(
-      Object.entries(options.config.reason_aliases).map(([k, v]) => [k.toLowerCase(), v]),
-    );
-  }
-
-  return options;
-};
-
-
 export const ModActionsPlugin = guildPlugin<ModActionsPluginType>()({
   name: "mod_actions",
 
   dependencies: () => [TimeAndDatePlugin, CasesPlugin, MutesPlugin, LogsPlugin],
-  configParser: (input) => zModActionsConfig.parse(input),
+  configParser: (input: any) => zModActionsConfig.parse(input),
   defaultOptions,
-  configPreprocessor,
 
   events: [CreateBanCaseOnManualBanEvt, CreateUnbanCaseOnManualUnbanEvt, PostAlertOnMemberJoinEvt, AuditLogEvents],
 
